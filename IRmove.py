42653d45f33cba415a7661374cf3cbf5a71c3061
lklinus2609 <lklinus0926@gmail.com>
2025-11-19 18:04:35 -0600

    got ir to work

--- IRmove.py
+++ IRmove.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python3
 import serial
 import time
-import numpy as np
 import RPi.GPIO as GPIO
 import asyncio
 
-sensor = 18
+# -------------------------------
+# GPIO Setup
+# -------------------------------
+SENSOR_1 = 17  
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(sensor,GPIO.IN)
+GPIO.setup(SENSOR_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-sensor2 = 17
+class IRMonitor:
+    def __init__(self, pin, samples=5):
+        self.pin = pin
+        self.samples = samples
+        self.state = 1  # 1 = beam unbroken, 0 = beam blocked
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(sensor2,GPIO.IN)
+    def read_stable(self):
+        vals = [GPIO.input(self.pin) for _ in range(self.samples)]
+        self.state = 0 if sum(vals) < self.samples / 2 else 1
+        return self.state
 
 
-async def IRcheck(ser):
-	await asyncio.sleep(1)
-	irValue = GPIO.input(sensor)
-	irValue2 = GPIO.input(sensor2)
-	serial.Serial('/dev/ttyACM0',115200).write(('<'+str(irValue)+','+str(irValue2)+'>').encode())
-	
-	if irValue:
-		print('ir1 NOT DETECTED')
-				
-	else:
-		print('ir1 DETECTED')
-	
-	if irValue2:
-		print('ir2 NOT DETECTED')
-				
-	else:
-		print('ir2 DETECTED')
+async def IRcheck(ir_monitor, ser):
+    ir_state = ir_monitor.read_stable()
+
+    ser.write(f"<{ir_state}>".encode())
+
+    if ir_state:
+        print("IR beam NOT DETECTED")
+    else:
+        print("IR beam DETECTED")
+
+    await asyncio.sleep(0.05)  # 100 ms loop for fast response
+
 
 async def main():
-	print('IR Sensor Ready')
-	ser = serial.Serial('/dev/ttyACM0',115200)
-	ser.reset_input_buffer()
-	try:
-		while True:
-			await IRcheck(ser)
-	except KeyboardInterrupt:
-		GPIO.cleanup()
+    print("IR Sensor Ready")
 
-asyncio.run(main())
+    ser = serial.Serial('/dev/ttyACM0', 115200)
+    ser.reset_input_buffer()
+
+    ir_monitor = IRMonitor(SENSOR_1, samples=20)
+
+    try:
+        while True:
+            await IRcheck(ir_monitor, ser)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        GPIO.cleanup()
+        ser.close()
+
+if __name__ == "__main__":
+    asyncio.run(main())
